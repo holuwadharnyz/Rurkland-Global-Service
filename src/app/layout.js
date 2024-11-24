80670d2941a28ac34dc04199c74ed039dc52ec1b
Joseph Daniel Olawale <missionarypwg@gmail.com>
2024-11-24 18:16:59 +0100

    Update layout.js
    Updated Website Title

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,7 +7,7 @@ import "./globals.css";
 // const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "RURKLAND",
+  title: "RURKLAND GLOBAL SERVICES",
   description: "Excellence in Every Drop, Progress in Every Partnership and Powering a Sustainable Future.",
 };
 
